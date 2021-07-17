0d944d83642b63c4057098863567d3d896a9ada4
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-07-17 14:37:35 +0800

    asset vault template - compiles w/ 0.15

---
+++ core-platform-contracts/asset-vault-template/examples/schema.rs
@@ -0,0 +1,23 @@
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+
+use asset_vault_template::msg::{ExecuteMsg, InstantiateMsg, InvestmentResponse, QueryMsg};
+use asset_vault_template::state::{Config, InvestmentInfo, Supply, TaxParameters};
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InvestmentResponse), &out_dir);
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(Config), &out_dir);
+    export_schema(&schema_for!(InvestmentInfo), &out_dir);
+    export_schema(&schema_for!(Supply), &out_dir);
+    export_schema(&schema_for!(TaxParameters), &out_dir);
+}
