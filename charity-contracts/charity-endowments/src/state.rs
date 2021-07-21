3a2d7f6200397b964292ff4a4fec6ec76505d186
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-07-21 20:24:39 +0800

    move SplitDetails into Charity Endowments SC state

--- charity-contracts/charity-endowments/src/state.rs
+++ charity-contracts/charity-endowments/src/state.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Env, Timestamp};
+use cosmwasm_std::{Addr, Decimal, Env, Timestamp};
 use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -9,6 +9,25 @@ use cw_storage_plus::{Item, Map};
 pub struct Config {
     pub owner: Addr,              // AP TEAM / DANO Address
     pub account_ledgers_sc: Addr, // Address of Account Ledgers SC
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct SplitDetails {
+    pub max: Decimal,
+    pub min: Decimal,
+    pub default: Decimal, // for when a split parameter is not provided
+}
+
+impl SplitDetails {
+    pub fn default() -> Self {
+        SplitDetails {
+            min: Decimal::zero(),
+            max: Decimal::one(),
+            default: Decimal::percent(50),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -20,6 +39,7 @@ pub struct Endowment {
     pub withdraw_before_maturity: bool, // endowment allowed to withdraw funds from locked acct before maturity date
     pub maturity_time: Option<u64>,     // datetime int of endowment maturity
     pub maturity_height: Option<u64>,   // block equiv of the maturity_datetime
+    pub split_to_liquid: SplitDetails   // set of max, min, and default Split paramenters to check user defined split input against
 }
 
 impl Endowment {
