86c61589b285389e578c5eb3f7a644f421e7b5f5
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-07-22 14:32:53 +0800

    rustfmt

--- charity-contracts/index-fund/src/state.rs
+++ charity-contracts/index-fund/src/state.rs
@@ -9,12 +9,12 @@ use cw20::{Balance, Cw20CoinVerified};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct Config {
-    pub owner: Addr, // DANO Address
-    pub account_ledgers_sc: Addr, // Address of Account Ledgers SC
-    pub terra_alliance: Vec<Addr>, // vec of terra charity alliance approved addresses
-    pub active_fund_index: Uint128, // index ID of the Active IndexFund
+    pub owner: Addr,                      // DANO Address
+    pub account_ledgers_sc: Addr,         // Address of Account Ledgers SC
+    pub terra_alliance: Vec<Addr>,        // vec of terra charity alliance approved addresses
+    pub active_fund_index: Uint128,       // index ID of the Active IndexFund
     pub fund_rotation_limit: Uint128, // how many blocks are in a rotation cycle for the active IndexFund
-    pub fund_member_limit: u32, // limit to number of members an IndexFund can have
+    pub fund_member_limit: u32,       // limit to number of members an IndexFund can have
     pub funding_goal: Option<Balance>, // donation funding limit to trigger early cycle of the Active IndexFund
     pub split_to_liquid_max: Decimal, // maximum % can split off into liquid account for an incoming donor deposit
     pub split_to_liquid_default: Decimal, // default % to split off into liquid account, if donor provided split is not present
@@ -36,8 +36,8 @@ pub struct IndexFund {
     // Defines the % to split off into liquid account, and if defined overrides all other split
     pub split_to_liquid_default: Option<Decimal>,
     // Used for one-off funds that have an end date (ex. disaster recovery funds)
-    pub expiry_time: Option<u64>,     // datetime int of index fund expiry
-    pub expiry_height: Option<u64>,   // block equiv of the expiry_datetime
+    pub expiry_time: Option<u64>,   // datetime int of index fund expiry
+    pub expiry_height: Option<u64>, // block equiv of the expiry_datetime
 }
 
 impl IndexFund {
@@ -57,5 +57,6 @@ impl IndexFund {
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
-pub const CURRENT_ROUND_DONATIONS: Map<String, GenericBalance> = Map::new("current_round_donations"); // EID mapped to Balances of donations
+pub const CURRENT_ROUND_DONATIONS: Map<String, GenericBalance> =
+    Map::new("current_round_donations"); // EID mapped to Balances of donations
 pub const FUNDS: Map<String, IndexFund> = Map::new("index_fund");
