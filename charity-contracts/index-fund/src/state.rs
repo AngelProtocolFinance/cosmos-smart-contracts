f0492ebad2326e2ae7b0310ff6ecaf5d915b1f78
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-07-22 14:28:46 +0800

    index fund - splits setup and expiary

--- charity-contracts/index-fund/src/state.rs
+++ charity-contracts/index-fund/src/state.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Coin, Uint128};
+use cosmwasm_std::{Addr, Coin, Decimal, Env, Timestamp, Uint128};
 use cw_storage_plus::{Item, Map};
 
 use cw20::{Balance, Cw20CoinVerified};
@@ -16,6 +16,8 @@ pub struct Config {
     pub fund_rotation_limit: Uint128, // how many blocks are in a rotation cycle for the active IndexFund
     pub fund_member_limit: u32, // limit to number of members an IndexFund can have
     pub funding_goal: Option<Balance>, // donation funding limit to trigger early cycle of the Active IndexFund
+    pub split_to_liquid_max: Decimal, // maximum % can split off into liquid account for an incoming donor deposit
+    pub split_to_liquid_default: Decimal, // default % to split off into liquid account, if donor provided split is not present
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
@@ -30,6 +32,28 @@ pub struct IndexFund {
     pub name: String,
     pub description: String,
     pub members: Vec<String>,
+    // Fund Specific: over-riding SC level setting to handle a fixed split value
+    // Defines the % to split off into liquid account, and if defined overrides all other split
+    pub split_to_liquid_default: Option<Decimal>,
+    // Used for one-off funds that have an end date (ex. disaster recovery funds)
+    pub expiry_time: Option<u64>,     // datetime int of index fund expiry
+    pub expiry_height: Option<u64>,   // block equiv of the expiry_datetime
+}
+
+impl IndexFund {
+    pub fn is_expired(&self, env: &Env) -> bool {
+        if let Some(expiry_height) = self.expiry_height {
+            if env.block.height > expiry_height {
+                return true;
+            }
+        }
+        if let Some(expiry_time) = self.expiry_time {
+            if env.block.time > Timestamp::from_seconds(expiry_time) {
+                return true;
+            }
+        }
+        false
+    }
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
