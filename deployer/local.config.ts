4f1e7dac339287744f3237cabd906af67c476e6f
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-08-24 14:38:35 +0800

    contracts deployment helper with terra.js

---
+++ deployer/local.config.ts
@@ -0,0 +1,9 @@
+export const Accounts: { [network: string]: any } = {
+  local: {
+    accounts: {
+      mnemonic:
+        "satisfy adjust timber high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn",
+    },
+  },
+  // define other networks
+};
