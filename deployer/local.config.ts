9fed34af989e4571c476e5f78b134677136cecc8
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-08-25 12:43:18 +0800

    remove old attempt

--- deployer/local.config.ts
+++
@@ -1,9 +0,0 @@
-export const Accounts: { [network: string]: any } = {
-  local: {
-    accounts: {
-      mnemonic:
-        "satisfy adjust timber high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn",
-    },
-  },
-  // define other networks
-};
