c33e8160502d2333ea325a93bc759cc537438cd4
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-08-28 11:04:10 +0800

    more flexible tests runs & contracts config file added

---
+++ test-suite/localConfig.ts
@@ -0,0 +1,10 @@
+export const accountsCodeId = 4;
+
+export const contracts = {
+	registrar: "terra18vd8fpwxzck93qlwghaj6arh4p7c5n896xzem5",
+	indexFund: "terra174kgn5rtw4kf6f938wm7kwh70h2v4vcfd26jlc",
+	anchorVault: "terra1sshdl5qajv0q0k6shlk8m9sd4lplpn6gggfr86",
+	endowmentContract1: "terra1tndcaqxkpc5ce9qee5ggqf430mr2z3pefe5wj6",
+	endowmentContract2: "terra1my3jvl6zs2n27648zngqrtw8pd23nrkrnw8hz6",
+	endowmentContract3: "terra1sndgzq62wp23mv20ndr4sxg6k8xcsudsy87uph",
+};
