41cba34e73ab7016e4d36a3ffa478830a140858b
Riki <43941751+curest0x1021@users.noreply.github.com>
2021-09-25 03:12:37 +0900

    Fixed spell error for guardian angels contract name

--- contracts/guardian-angels/guardian-angels-cw3/src/contract.rs
+++ contracts/guardian-angels/guardian-angels-cw3/src/contract.rs
@@ -22,7 +22,7 @@ use cw_storage_plus::Bound;
 use std::cmp::Ordering;
 
 // version info for migration info
-const CONTRACT_NAME: &str = "guardian-angel-multisig";
+const CONTRACT_NAME: &str = "guardian-angels-multisig";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
