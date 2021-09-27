87c6ea4678fe1a2d3d8c6340fcc3732a4fd04fa0
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-09-27 08:45:15 +0800

    rustfmt & rebuild wasms

--- contracts/vaults/anchor/src/executers.rs
+++ contracts/vaults/anchor/src/executers.rs
@@ -352,7 +352,9 @@ pub fn harvest(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, C
 
         // proceed to shuffle balances if we have a non-zero amount
         if locked_deposit_amount > Uint128::zero() {
-            let transfer_amt = locked_deposit_amount.checked_mul(harvest_percent * config.tax_per_block).unwrap();
+            let transfer_amt = locked_deposit_amount
+                .checked_mul(harvest_percent * config.tax_per_block)
+                .unwrap();
             let taxes_owed = transfer_amt * registrar_config.tax_rate;
 
             // lower locked balance
