c3862e2f435d490cc29092bad398f692f362eecb
Riki <43941751+curest0x1021@users.noreply.github.com>
2021-10-12 18:43:14 +0900

    Fix path issue

--- test-suite/src/processes/setupContracts/testnet.ts
+++ test-suite/src/processes/setupContracts/testnet.ts
@@ -104,49 +104,49 @@ async function setup(
   const registrarCodeId = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/registrar.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/registrar.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${registrarCodeId}`);
   
   process.stdout.write("Uploading Anchor Vault Wasm");
   const vaultCodeId = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/anchor.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/anchor.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${vaultCodeId}`);
   
   process.stdout.write("Uploading Index Fund Wasm");
   const fundCodeId = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/index_fund.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/index_fund.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${fundCodeId}`);
   
   process.stdout.write("Uploading Accounts Wasm");
   const accountsCodeId = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/accounts.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/accounts.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${accountsCodeId}`);
 
   process.stdout.write("Uploading CW4 Group Wasm");
   const cw4Group = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/cw4_group.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/cw4_group.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${cw4Group}`);
 
   process.stdout.write("Uploading Guardian Angels MultiSig Wasm");
   const guardianAngelMultiSig = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/guardian_angels_multisig.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/guardian_angels_multisig.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${guardianAngelMultiSig}`);
 
   process.stdout.write("Uploading AP Team MultiSig Wasm");
   const apTeamMultiSig = await storeCode(
     terra,
     apTeam,
-    path.resolve(__dirname, "../../artifacts/ap_team_multisig.wasm"));
+    path.resolve(__dirname, "../../../../artifacts/ap_team_multisig.wasm"));
   console.log(chalk.green(" Done!"), `${chalk.blue("codeId")}=${apTeamMultiSig}`);
 
   // Step 2. Instantiate the key contracts
