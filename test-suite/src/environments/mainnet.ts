c6d78641ee7273bb5fda566976537b4e8e905bc9
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-10-22 10:14:12 +0800

    lower the min gas prices

--- test-suite/src/environments/mainnet.ts
+++ test-suite/src/environments/mainnet.ts
@@ -44,7 +44,7 @@ function initialize() {
   terra = new LCDClient({
     URL: config.networkInfo.url,
     chainID: config.networkInfo.chainId,
-    gasPrices: { uusd: 0.5 },
+    gasPrices: { uusd: 0.15 },
     gasAdjustment: 1.2,
   });
   apTeam = terra.wallet(new MnemonicKey({mnemonic: config.mnemonicKeys.apTeam}));
