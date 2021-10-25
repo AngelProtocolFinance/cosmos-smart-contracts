5d839150c4eed184013e578781a7c731e800c81d
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-10-25 22:01:45 +0800

    last anc refs removed

--- contracts/halo/staking/src/querier.rs
+++ contracts/halo/staking/src/querier.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::{to_binary, Addr, QuerierWrapper, QueryRequest, StdResult, Was
 use cw20::{Cw20QueryMsg, MinterResponse};
 
 /// Query asset price igonoring price age
-pub fn query_anc_minter(querier: &QuerierWrapper, halo_token: Addr) -> StdResult<String> {
+pub fn query_halo_minter(querier: &QuerierWrapper, halo_token: Addr) -> StdResult<String> {
     let res: MinterResponse = querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
         contract_addr: halo_token.to_string(),
         msg: to_binary(&Cw20QueryMsg::Minter {})?,
