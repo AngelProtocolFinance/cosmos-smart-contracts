abacf51217a820ba937ae040e67966fea3db4ea9
guorong <dudurong009@gmail.com>
2022-03-14 10:55:16 +0800

    Update the storage with "cw-storage-plus" in "halo/community" contract

--- contracts/halo/community/src/state.rs
+++ contracts/halo/community/src/state.rs
@@ -1,10 +1,8 @@
+use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{Addr, StdResult, Storage, Uint128};
-use cosmwasm_storage::{singleton, singleton_read};
-
-static KEY_CONFIG: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
@@ -13,10 +11,12 @@ pub struct Config {
     pub spend_limit: Uint128, // spend limit per each `spend` request
 }
 
+pub const CONFIG: Item<Config> = Item::new("config");
+
 pub fn store_config(storage: &mut dyn Storage, config: &Config) -> StdResult<()> {
-    singleton(storage, KEY_CONFIG).save(config)
+    CONFIG.save(storage, config)
 }
 
 pub fn read_config(storage: &dyn Storage) -> StdResult<Config> {
-    singleton_read(storage, KEY_CONFIG).load()
+    CONFIG.load(storage)
 }
