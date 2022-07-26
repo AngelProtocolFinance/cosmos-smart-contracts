388a7229127740cf33911d2a7273163fb5d35259
Andrey <andrey@angelprotocol.io>
2022-07-26 19:00:56 +0800

    Revert "new cw3/cw4 setup applied to v1.6 contracts"
    This reverts commit dd13a33c111d6eb52b39f20b73ea95943a6932d7.

--- contracts/core/accounts/src/executers.rs
+++ contracts/core/accounts/src/executers.rs
@@ -34,6 +34,7 @@ pub fn new_cw4_group_reply(
     _env: Env,
     msg: SubMsgResult,
 ) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
     match msg {
         SubMsgResult::Ok(subcall) => {
             let mut group_addr = String::from("");
@@ -49,10 +50,8 @@ pub fn new_cw4_group_reply(
                 }
             }
 
-            // Register the new CW4 contract addr on success Reply
-            let mut config = CONFIG.load(deps.storage)?;
-            config.cw4_group = Some(deps.api.addr_validate(&group_addr)?);
-            CONFIG.save(deps.storage, &config)?;
+            // Register the new Endowment on success Reply
+            let _addr = deps.api.addr_validate(&group_addr)?;
 
             let registrar_config: RegistrarConfigResponse =
                 deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
@@ -110,30 +109,9 @@ pub fn new_cw3_multisig_reply(
             endowment.owner = deps.api.addr_validate(&multisig_addr)?;
             ENDOWMENT.save(deps.storage, &endowment)?;
 
-            let config = CONFIG.load(deps.storage)?;
-
-            Ok(Response::default()
-                .add_attribute("cw3_addr", multisig_addr.clone())
-                .add_submessages([
-                    SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
-                        contract_addr: config.cw4_group.as_ref().unwrap().to_string(),
-                        msg: to_binary(&angel_core::messages::cw4_group::ExecuteMsg::AddHook {
-                            addr: multisig_addr.clone(),
-                        })?,
-                        funds: vec![],
-                    })),
-                    SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
-                        contract_addr: config.cw4_group.unwrap().to_string(),
-                        msg: to_binary(
-                            &angel_core::messages::cw4_group::ExecuteMsg::UpdateAdmin {
-                                admin: Some(multisig_addr),
-                            },
-                        )?,
-                        funds: vec![],
-                    })),
-                ]))
+            Ok(Response::default())
         }
-        SubMsgResult::Err(err) => Err(ContractError::Std(StdError::GenericErr { msg: err })),
+        SubMsgResult::Err(_) => Err(ContractError::AccountNotCreated {}),
     }
 }
 
