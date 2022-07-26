26d43b79ebeef70b677d3911edc62e60f142e59f
Andrey <andrey@angelprotocol.io>
2022-07-26 13:39:44 +0800

    fix mainnet currencies

--- src/processes/setup/core/mainnet.ts
+++ src/processes/setup/core/mainnet.ts
@@ -166,7 +166,7 @@ async function setup(
       tax_rate: tax_rate,
       default_vault: undefined,
       accepted_tokens: {
-        native: ['ibc/EAC38D55372F38F1AFD68DF7FE9EF762DCF69F26520643CF3F9D292A738D8034', 'ujunox'],
+        native: ['ibc/EAC38D55372F38F1AFD68DF7FE9EF762DCF69F26520643CF3F9D292A738D8034', 'ujuno'],
         cw20: [],
       }
     }
