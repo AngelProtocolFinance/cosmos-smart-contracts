f757107eae840398abfa67009cd2cd6cb087e365
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2022-08-01 10:54:16 +0800

    Update contracts/normalized-endowment/accounts/src/state.rs

--- contracts/normalized-endowment/accounts/src/state.rs
+++ contracts/normalized-endowment/accounts/src/state.rs
@@ -12,7 +12,7 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     pub owner: Addr, // DANO/AP Team Address
     pub registrar_contract: Addr,
-    pub deposit_approved: bool, // DANO has approved to recDOeive donations & transact
+    pub deposit_approved: bool, // DANO has approved to receive donations & transact
     pub withdraw_approved: bool, // DANO has approved to withdraw funds
     pub pending_redemptions: Option<u64>,
     pub last_earnings_harvest: u64,
