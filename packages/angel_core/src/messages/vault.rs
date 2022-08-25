dec31a40ce2856a479e8abb5017084cc92cb09eb
duguorong <dudurong009@gmail.com>
2022-08-25 10:31:27 -0700

    Remove the unnecessary entries from "loop vault" contract

--- packages/angel_core/src/messages/vault.rs
+++ packages/angel_core/src/messages/vault.rs
@@ -38,18 +38,8 @@ pub enum ExecuteMsg {
         endowment_id: u32,
     },
     Claim {},
-    DistributeClaim {
-        reward_token_bal_before: Uint128,
-    },
     Withdraw(AccountWithdrawMsg),
     Harvest {},
-    HarvestSwap {
-        token1_denom_bal_before: Uint128,
-        token2_denom_bal_before: Uint128,
-    },
-    DistributeHarvest {
-        output_token_bal_before: Uint128,
-    },
     AddLiquidity {
         endowment_id: u32,
         in_asset_info: terraswap::asset::AssetInfo,
