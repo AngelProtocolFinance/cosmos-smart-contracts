07ef59bd58e31737d0fb15e06590b79cd749b221
duguorong <dudurong009@gmail.com>
2022-11-17 22:28:46 -0800

    Update the Endowment migrate logic in "accounts" contract

--- contracts/core/accounts/src/state.rs
+++ contracts/core/accounts/src/state.rs
@@ -25,6 +25,31 @@ pub struct Config {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
+pub struct OldEndowment {
+    pub owner: Addr,
+    pub name: String,           // name of the Endowment
+    pub categories: Categories, // SHOULD NOT be editable for now (only the Config.owner, ie via the Gov contract or AP CW3 Multisig can set/update)
+    pub tier: Option<u8>, // SHOULD NOT be editable for now (only the Config.owner, ie via the Gov contract or AP CW3 Multisig can set/update)
+    pub endow_type: EndowmentType,
+    pub logo: Option<String>,
+    pub image: Option<String>,
+    pub status: EndowmentStatus,
+    pub deposit_approved: bool, // approved to receive donations & transact
+    pub withdraw_approved: bool, // approved to withdraw funds
+    pub withdraw_before_maturity: bool, // endowment allowed to withdraw funds from locked acct before maturity date
+    pub maturity_time: Option<u64>,     // datetime int of endowment maturity
+    pub maturity_height: Option<u64>,   // block equiv of the maturity_datetime
+    pub strategies: AccountStrategies, // vaults and percentages for locked/liquid accounts donations where auto_invest == TRUE
+    pub oneoff_vaults: OneOffVaults, // vaults not covered in account startegies (more efficient tracking of vaults vs. looking up allll vaults)
+    pub rebalance: RebalanceDetails, // parameters to guide rebalancing & harvesting of gains from locked/liquid accounts
+    pub kyc_donors_only: bool, // allow owner to state a preference for receiving only kyc'd donations (where possible)
+    pub pending_redemptions: u8, // number of vault redemptions currently pending for this endowment
+    pub copycat_strategy: Option<u32>, // endowment ID to copy their strategy
+    pub proposal_link: Option<u64>, // link back the CW3 Proposal that created an endowment
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub struct Endowment {
     pub owner: Addr,            // address that originally setup the endowment account
     pub name: String,           // name of the Endowment
