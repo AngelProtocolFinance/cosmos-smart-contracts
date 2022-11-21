3361537757610b5aacb357c9ee33873ce2109717
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2022-11-21 10:18:22 +0800

    gift cards are claimable if not assigned on deposit of funds

--- contracts/accessory/gift-cards/src/msg.rs
+++ contracts/accessory/gift-cards/src/msg.rs
@@ -1,3 +1,4 @@
+use crate::state::Config;
 use angel_core::messages::accounts::DepositMsg;
 use angel_core::structs::GenericBalance;
 use cosmwasm_schema::QueryResponses;
@@ -12,13 +13,14 @@ pub struct MigrateMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub registrar_contract: String,
+    pub keeper: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ReceiveMsg {
     // Add tokens sent for a specific address
-    TopUp { to_address: String },
+    Deposit { to_address: Option<String> },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -26,8 +28,14 @@ pub enum ReceiveMsg {
 pub enum ExecuteMsg {
     Receive(Cw20ReceiveMsg),
     // Add tokens sent for a specific address
-    TopUp {
-        to_address: String,
+    Deposit {
+        to_address: Option<String>,
+    },
+    // Claim a deposited Asset to Addr Balance
+    // for now, this is only doable by Keeper Addr
+    Claim {
+        deposit: u64,
+        recipient: String,
     },
     // Spend token/amount specified from sender balance to Endowment
     Spend {
@@ -36,6 +44,7 @@ pub enum ExecuteMsg {
     },
     UpdateConfig {
         owner: Option<String>,
+        keeper: Option<String>,
         registrar_contract: Option<String>,
     },
 }
@@ -44,4 +53,6 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     #[returns(GenericBalance)]
     Balance { address: String },
+    #[returns(Config)]
+    Config {},
 }
