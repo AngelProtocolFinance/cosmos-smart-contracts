5f568a278340090f189e5d2f061720109212bc73
duguorong <dudurong009@gmail.com>
2022-12-07 09:53:49 -0800

    Remove the "receive_ibc_response" entry from "accounts" contract

--- contracts/core/accounts/src/executers.rs
+++ contracts/core/accounts/src/executers.rs
@@ -27,7 +27,6 @@ use cw20::{Balance, Cw20Coin, Cw20CoinVerified, Cw20ExecuteMsg};
 use cw4::Member;
 use cw_asset::{Asset, AssetInfo, AssetInfoBase, AssetUnchecked};
 use cw_utils::{Duration, Expiration};
-use ica_vaults::ibc_msg::ReceiveIbcResponseMsg;
 
 pub fn cw3_reply(deps: DepsMut, _env: Env, msg: SubMsgResult) -> Result<Response, ContractError> {
     match msg {
@@ -60,22 +59,6 @@ pub fn cw3_reply(deps: DepsMut, _env: Env, msg: SubMsgResult) -> Result<Response
         }
         SubMsgResult::Err(err) => Err(ContractError::Std(StdError::GenericErr { msg: err })),
     }
-}
-
-pub fn execute_receive_ibc_response(
-    deps: DepsMut,
-    _env: Env,
-    info: MessageInfo,
-    resp: ReceiveIbcResponseMsg,
-) -> Result<Response, ContractError> {
-    // only the ibc controller can send this type message as callback
-    let config = CONFIG.load(deps.storage)?;
-    if !config.ibc_controller.eq(&info.sender) {
-        return Err(ContractError::Unauthorized {});
-    }
-    Ok(Response::new()
-        .add_attribute("action", "receive_ibc_callback")
-        .add_attribute("id", resp.id))
 }
 
 pub fn create_endowment(
--- packages/angel_core/src/messages/accounts.rs
+++ packages/angel_core/src/messages/accounts.rs
@@ -7,7 +7,6 @@ use cw20::Cw20ReceiveMsg;
 use cw4::Member;
 use cw_asset::{Asset, AssetInfo, AssetUnchecked};
 use cw_utils::{Expiration, Threshold};
-use ica_vaults::ibc_msg::ReceiveIbcResponseMsg;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -26,8 +25,6 @@ pub struct InstantiateMsg {
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     Receive(Cw20ReceiveMsg),
-    // catch ICA msg responses from ICA Controller
-    ReceiveIbcResponse(ReceiveIbcResponseMsg),
     // Add tokens sent for a specific account
     Deposit(DepositMsg),
     /// reinvest vault assets from Liquid to Locked
