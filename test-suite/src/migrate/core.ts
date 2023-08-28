06ef2017b38857247eeb0d1fe0adb9cbf1d0ce7b
Andrey <SovereignAndrey>
2023-08-28 15:43:24 +0800

    update packages; fix un-commented out migrate script lines;

--- test-suite/src/migrate/core.ts
+++ test-suite/src/migrate/core.ts
@@ -38,12 +38,12 @@ export async function migrateCore(
   axelarIbcChannel: string,
   axelarChainId: string
 ): Promise<void> {
-  await storeAndMigrateContract(juno, apTeam, registrar, "registrar.wasm", {
-    axelar_gateway: axelarGateway,
-    axelar_ibc_channel: axelarIbcChannel,
-    axelar_chain_id: axelarChainId,
-    accounts_settings_controller: settingsController,
-  });
+  // await storeAndMigrateContract(juno, apTeam, registrar, "registrar.wasm", {
+  //   axelar_gateway: axelarGateway,
+  //   axelar_ibc_channel: axelarIbcChannel,
+  //   axelar_chain_id: axelarChainId,
+  //   accounts_settings_controller: settingsController,
+  // });
   // await storeAndMigrateContract(juno, apTeam, accounts, "accounts.wasm");
   // await storeAndMigrateContractViaCw3(juno, apTeam, cw3ApTeam, accounts, "accounts.wasm");
   // await storeAndMigrateContract(juno, apTeam, settingsController, 'accounts_settings_controller.wasm');
